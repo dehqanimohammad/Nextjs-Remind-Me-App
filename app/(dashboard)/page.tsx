ab965484b0de553aef7b35251463b18af199712e
dehqanimohammad <dehqaniimohamad@gmail.com>
2024-06-13 13:32:11 +0330

    initial

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <div>can't find username</div>;
+  }
+  return (
+    <div className="flex w-full">
+      <div className="text-4xl font-bold">
+        Welcome,,
+        <br /> {user.username}
+      </div>
+    </div>
+  );
+}
---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { DesktopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
+function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className="border dark:border-neutral-800 dark:bg-black">
+        <TabsTrigger value="light" onClick={(e) => setTheme("light")}>
+          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+        <TabsTrigger value="dark" onClick={(e) => setTheme("dark")}>
+          <MoonIcon className="h-[1.2rem] w-[1.2rem] transition-all dark:rotate-0 rotate-90" />
+        </TabsTrigger>
+        <TabsTrigger value="system" onClick={(e) => setTheme("system")}>
+          <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+}
+
+export default ThemeSwitcher;
