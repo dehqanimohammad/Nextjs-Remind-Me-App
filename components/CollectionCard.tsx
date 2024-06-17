d0d8033f33cb0430b247c42a998f006d514de4d1
dehqanimohammad <dehqaniimohamad@gmail.com>
2024-06-17 15:22:47 +0330

    finishing taskcard

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Collection, Task } from "@prisma/client";
-import React, { useState, useTransition } from "react";
+import React, { useMemo, useState, useTransition } from "react";
 import {
   Collapsible,
   CollapsibleContent,
@@ -32,6 +32,8 @@ import {
 import { deleteCollection } from "@/actions/collection";
 import { toast } from "./ui/use-toast";
 import { useRouter } from "next/navigation";
+import CreateTaskDialog from "./CreateTaskDialog";
+import TaskCard from "./TaskCard";
 
 interface Props {
   collection: Collection & {
@@ -43,6 +45,7 @@ function CollectionCard({ collection }: Props) {
   const [isOpen, setIsOpen] = useState(true);
   const router = useRouter();
   const [isLoading, startTransition] = useTransition();
+  const [showCreateModal, setShowCreateModal] = useState(false);
 
   const tasks = collection.tasks;
 
@@ -63,66 +66,102 @@ function CollectionCard({ collection }: Props) {
     }
   };
 
+  const tasksDone = useMemo(() => {
+    return collection.tasks.filter((task) => task.done).length;
+  }, [collection.tasks]);
+
+  const progress =
+    collection.tasks.length === 0
+      ? 0
+      : (tasksDone / collection.tasks.length) * 100;
+
   return (
-    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <CollapsibleTrigger asChild>
-        <Button
-          variant={"ghost"}
-          className={cn(
-            "flex w-full justify-between p-6",
-            isOpen && "rounded-b-none",
-            CollectionColors[collection.color as CollectionColor]
+    <>
+      <CreateTaskDialog
+        open={showCreateModal}
+        setOpen={setShowCreateModal}
+        collection={collection}
+      />
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger asChild>
+          <Button
+            variant={"ghost"}
+            className={cn(
+              "flex w-full justify-between p-6",
+              isOpen && "rounded-b-none",
+              CollectionColors[collection.color as CollectionColor]
+            )}
+          >
+            <span className="text-white font-bold">{collection.name}</span>
+            {!isOpen && <CaretDownIcon className="h-6 w-6" />}
+            {isOpen && <CaretUpIcon className="h-6 w-6" />}
+          </Button>
+        </CollapsibleTrigger>
+        <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-950 shadow-lg">
+          {tasks.length === 0 && (
+            <Button
+              variant={"ghost"}
+              className="flex items-center justify-center gap-1 p-8 py-12 rounded-none"
+              onClick={() => setShowCreateModal(true)}
+            >
+              <p>There are no tasks yet:</p>
+              <span
+                className={cn(
+                  "text-sm bg-clip-text text-transparent",
+                  CollectionColors[collection.color as CollectionColor]
+                )}
+              >
+                Create one
+              </span>
+            </Button>
           )}
-        >
-          <span className="text-white font-bold">{collection.name}</span>
-          {!isOpen && <CaretDownIcon className="h-6 w-6" />}
-          {isOpen && <CaretUpIcon className="h-6 w-6" />}
-        </Button>
-      </CollapsibleTrigger>
-      <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-950 shadow-lg">
-        {tasks.length === 0 && <div>No tasks</div>}
-        {tasks.length > 0 && (
-          <>
-            <Progress className="rounded-none" value={80} />
-            <div className="p-4 gap-3 flex flex-col">
-              {tasks.map((task) => (
-                <div>task</div>
-              ))}
-            </div>
-          </>
-        )}
-        <Separator />
-        <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center">
-          <p>Created at {collection.createdAt.toDateString()}</p>
-          {isLoading && <div>Deleting ...</div>}
-          {!isLoading && (
-            <div>
-              <Button variant={"ghost"} size={"icon"}>
-                <PlusCircledIcon />
-              </Button>
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Button variant={"ghost"} size={"icon"}>
-                    <TrashIcon />
-                  </Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogTitle>Are you sure???</AlertDialogTitle>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction
-                      onClick={() => startTransition(removeColleection)}
-                    >
-                      Delete
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </div>
+          {tasks.length > 0 && (
+            <>
+              <Progress className="rounded-none" value={progress} />
+              <div className="p-4 gap-3 flex flex-col">
+                {tasks.map((task) => (
+                  <TaskCard key={task.id} task={task} />
+                ))}
+              </div>
+            </>
           )}
-        </footer>
-      </CollapsibleContent>
-    </Collapsible>
+          <Separator />
+          <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center">
+            <p>Created at {collection.createdAt.toDateString()}</p>
+            {isLoading && <div>Deleting ...</div>}
+            {!isLoading && (
+              <div>
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  onClick={() => setShowCreateModal(true)}
+                >
+                  <PlusCircledIcon />
+                </Button>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"}>
+                      <TrashIcon />
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogTitle>Are you sure???</AlertDialogTitle>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction
+                        onClick={() => startTransition(removeColleection)}
+                      >
+                        Delete
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+            )}
+          </footer>
+        </CollapsibleContent>
+      </Collapsible>
+    </>
   );
 }
 
